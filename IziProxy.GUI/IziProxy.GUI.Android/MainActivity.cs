9cafb4ba94c23e16d3503f5abd7b7c7ba71ff4be
Алексей <6alexam@gmail.com>
2026-05-28 23:26:36 +0400

    Подготовка к добавлению GUI

---
+++ IziProxy.GUI/IziProxy.GUI.Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+
+namespace IziProxy.GUI.Android;
+
+[Activity(
+    Label = "IziProxy.GUI.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity
+{
+}
---
+++ IziProxy.GUI/IziProxy.GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace IziProxy.GUI.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
