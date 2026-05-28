c3f933b072c6c4979b40cd3223aab94260a74a68
Алексей <6alexam@gmail.com>
2026-05-29 02:55:46 +0400

    выбор ssh-ключа из проводника

--- IziProxy.GUI/IziProxy.GUI/Views/DeployView.axaml.cs
+++ IziProxy.GUI/IziProxy.GUI/Views/DeployView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using IziProxy.GUI.ViewModels;
 
 namespace IziProxy.GUI.Views;
 
@@ -7,5 +9,22 @@ public partial class DeployView : UserControl
     public DeployView()
     {
         InitializeComponent();
+    }
+
+    public async void BrowseSshKey()
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        if (topLevel == null) return;
+
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Выберите приватный SSH-ключ",
+            AllowMultiple = false
+        });
+
+        if (files.Count > 0 && DataContext is DeployViewModel vm)
+        {
+            vm.SshKeyPath = files[0].Path.LocalPath;
+        }
     }
 }
